f8d1c954044f2b757be66beba098087a34dacdbe
MIGALHA <gabrielmigalhabarros@gmail.com>
2025-04-02 22:22:35 -0300

    fix: fix the hour pointer that wasn't properly aligned
    feature: add toggle to switch between local time and UTC time
    
    feature: add miliseconds check to make the seconds pointer smoother

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let useUTC = true;
+
+let buttonbox = document
+  .querySelector(".buttons_box button")
+  .addEventListener("click", function () {
+    useUTC = !useUTC;
+
+    document.querySelector("#circle_path + text textPath").textContent = useUTC
+      ? "UTC TIME"
+      : "LOCAL TIME";
+  });
+
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
@@ -5,24 +17,27 @@ let hours = 0;
 setInterval(function getTime() {
   let currentTime = new Date();
 
-  seconds = currentTime.getSeconds();
+  miliseconds = currentTime.getUTCMilliseconds();
+  milisecondsAngle = (360 / 1000) * miliseconds;
+
+  seconds = currentTime.getUTCSeconds();
   secondsAngle = (360 / 60) * seconds;
 
-  minutes = currentTime.getMinutes();
+  minutes = currentTime.getUTCMinutes();
   minutesAngle = (360 / 60) * minutes;
 
-  hours = currentTime.getHours();
-  hoursAngle = (360 / 60) * hours;
+  hours = useUTC ? currentTime.getUTCHours() : currentTime.getHours();
+  hoursAngle = (360 / 12) * hours;
 
-  document.getElementById(
-    "seconds_pointer"
-  ).style.transform = `rotate(${secondsAngle}deg)`;
+  document.getElementById("seconds_pointer").style.transform = `rotate(${
+    secondsAngle + milisecondsAngle / 60
+  }deg)`;
 
   document.getElementById("minute_pointer").style.transform = `rotate(${
     minutesAngle + secondsAngle / 60
   }deg)`;
 
   document.getElementById("hour_pointer").style.transform = `rotate(${
-    hoursAngle + minutesAngle / 60
+    hoursAngle + minutesAngle / 12
   }deg)`;
-}, 1000);
+}, 10);
